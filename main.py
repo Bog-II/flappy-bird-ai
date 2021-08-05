bdda7d1a463203939028809cceedda705ed65e28
Bogdan <bogsacha@gmail.com>
2021-08-05 10:56:27 +0200

    rename __init__.py to main.py

--- main.py
+++ main.py
@@ -4,21 +4,10 @@ import time
 import os
 import random
 
-import sys
-
 from load_images import load_image
 
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 800
+from constants import (WINDOW_WIDTH,
+                       WINDOW_HEIGHT)
 
-IMAGE_DIRECTORY_PATH = "images"
 
-BIRD_IMAGES = [
-    load_image(IMAGE_DIRECTORY_PATH, "bird1.png"),
-    load_image(IMAGE_DIRECTORY_PATH, "bird2.png"),
-    load_image(IMAGE_DIRECTORY_PATH, "bird3.png")
-]
 
-PIPE_IMAGE = load_image(IMAGE_DIRECTORY_PATH, "pipe.png")
-BASE_IMAGE = load_image(IMAGE_DIRECTORY_PATH, "base.png")
-BG_IMAGE = load_image(IMAGE_DIRECTORY_PATH, "bg.png")
