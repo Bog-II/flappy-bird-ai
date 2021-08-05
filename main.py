6cedca11a049e583836c6345cad26c55d1dc6db2
Bogdan <bogsacha@gmail.com>
2021-08-05 14:35:20 +0200

    draw_window()

--- main.py
+++ main.py
@@ -4,10 +4,38 @@ import time
 import os
 import random
 
-from load_images import load_image
+from images import BG_IMAGE, BASE_IMAGE
+from classes import Bird
 
-from constants import (WINDOW_WIDTH,
-                       WINDOW_HEIGHT)
+from constants import (WINDOW_WIDTH, WINDOW_HEIGHT)
 
 
 
+def draw_window(window, bird):
+    window.blit(BG_IMAGE, (0, 0))
+    bird.draw(window)
+    pygame.display.update()
+
+
+def main():
+    bird = Bird(200, 200)
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    clock = pygame.time.Clock()
+
+    run = True
+
+    while run:
+        clock.tick(30)
+        for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+            run = False
+
+        bird.move()
+        draw_window(window, bird)
+        # draw_window(window)
+    
+
+    pygame.quit()
+    quit()
+
+main()
