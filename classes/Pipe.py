4ed059f87cb28149fa21a5c8d31ea631044c0956
Bogdan <bogsacha@gmail.com>
2021-08-05 15:19:50 +0200

    add basic Pip methods

--- classes/Pipe.py
+++ classes/Pipe.py
@@ -1,2 +1,36 @@
+import pygame
+import random
+
+from images import PIPE_IMAGE_TOP, PIPE_IMAGE_BOTTOM
+from constants import WINDOW_HEIGHT
+
 
 class Pipe:
+    GAP = 200
+    VELOCITY = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.gap = 100
+
+        self.top = 0
+        self.bottom = 0
+
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, WINDOW_HEIGHT - 100)
+        self.top = self.height - PIPE_IMAGE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.vel
+
+    def draw(self, window):
+        window.blit(self.PIPE_IMAGE_TOP, (self.x, self.top))
+        window.blit(self.PIPE_IMAGE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird):
+      
